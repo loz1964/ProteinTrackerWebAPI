5e674a4038837cfeda9f3c06339d1987368c9b86
Laurence Rimington <lozcovariant@gmail.com>
2016-07-06 02:24:24 +0100

    webApi
    protein tracker WebApi

---
+++ ProteinTrackerWebAPI/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using ProteinTrackerWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProteinTrackerWebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        private UserRepository repository = new UserRepository();
+        public string Delete(int id)
+        {
+            string username;
+            if (repository.GetById(id) != null)
+            {
+                username = repository.GetById(id).Name;
+            }else
+            {
+                username = id.ToString();
+            }
+
+             
+            return (repository.Delete(id) >= 0)?  "successfully deleted user " + username:
+             "Failed to Delete user " + username;
+        }
+
+        public int Put(int id, [FromBody] int amount)
+        {
+            var user = repository.GetById(id);
+            if (user == null)
+                return -1;
+            user.Total += amount;
+            repository.Save(user);
+            return user.Total;
+        }
+
+
+        public int Post([FromBody] CreateUserRequest request)
+        {
+            var user = new User { Goal = request.Goal, Name = request.Name, Total = 0 };
+            repository.Add(user);
+
+            return user.UserId;
+        }
+
+
+        public IEnumerable<User> Get()
+        {
+            return new List<User>(repository.GetAll());
+        }
+        public class CreateUserRequest
+        {
+            public int Goal { get; set; }
+            public string Name { get; set; }
+
+
+        }
+
+    }
+}
---
+++ ProteinTrackerWebAPI/Models/UserRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinTrackerWebAPI.Models
+{
+    
+    
+        public interface IUserRepository
+        {
+            void Add(User user);
+            ReadOnlyCollection<User> GetAll();
+            User GetById(int id);
+            void Save(User updatedUser);
+        }
+
+        public class UserRepository : IUserRepository
+        {
+            private static readonly List<User> users = new List<User>();
+            private static int nextId = 0;
+
+            public void Add(User user)
+            {
+                user.UserId = nextId;
+                nextId++;
+                users.Add(user);
+            
+            }
+
+
+            public int Delete(int userId)
+            {
+                User user = users.SingleOrDefault(u => u.UserId == userId);
+                if (user == null) { return -1; }
+                users.Remove(user);
+                return userId;
+             }
+
+            public ReadOnlyCollection<User> GetAll()
+            {
+                return users.AsReadOnly();
+            }
+
+            public User GetById(int id)
+            {
+                var user = users.SingleOrDefault(u => u.UserId == id);
+                if (user == null)
+                    return null;
+
+                return new User { Goal = user.Goal, Name = user.Name, UserId = user.UserId, Total = user.Total };
+            }
+
+            public void Save(User updatedUser)
+            {
+                var originalUser = users.SingleOrDefault(u => u.UserId == updatedUser.UserId);
+                if (originalUser == null)
+                    return;
+
+                originalUser.Name = updatedUser.Name;
+                originalUser.Total = updatedUser.Total;
+                originalUser.Goal = updatedUser.Goal;
+            }
+        }
+    
+}
